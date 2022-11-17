d32208ff05a85091a3143ec1b3932ca4f134ecf8
gg-mike <michal.g.grygiel@gmail.com>
2022-11-17 14:36:25 +0100

    Fix

--- src/lambda/auth.py
+++ src/lambda/auth.py
@@ -1,33 +1,18 @@
 import requests
 
 
-def get_effect(token):
+def isValidToken(token):
     response = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={token.split(' ')[1]}")
     if response.status_code == 200:
-        return "Allow"
-    return "Deny"
+        return True
+    return False
 
 
 def handler(event, context):
-    # effect = "Deny"
-    # routeArn = "placeholder"
-    # try:
-    #     effect = get_effect(event["headers"]["authorization"])
-    #     routeArn = event["routeArn"]
-    # except Exception as e:
-    #     print(e)
+    isAuthorized = False
+    try:
+        isAuthorized = isValidToken(event["headers"]["authorization"])
+    except Exception as e:
+        print(e)
     
-    response = {
-        "principalId": "xyz",
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Action": "execute-api:Invoke",
-                    "Effect": "Allow",
-                    "Resource": event["routeArn"]
-                }
-            ]
-        }
-    }
-    return response
+    return { "isAuthorized": isAuthorized }
